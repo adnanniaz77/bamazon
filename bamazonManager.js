8ca3c08073a3599dbb90f706a87d491486bbf3f1
Adnan Niaz <adnanniaz77@yahoo.com>
2019-12-08 10:53:40 -0800

    started bamazon.js with inquirer and list choice

---
+++ bamazonManager.js
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+const functions = require("./functions.js");
+
+inquirer
+    .prompt({
+        type: "list",
+        message: "What do you want to do? \n ========================\n",
+        name: "managerChoice",
+        choices: [
+            "View Products for Sale",
+            "View Low Inventory",
+            "Add to Inventory",
+            "Add New product",
+            "EXIT"
+        ]
+    })
+    .then(res => {
+        if (res.managerChoice === "View Products for Sale") {
+            functions.showAllProducts();
+        } else if (res.managerChoice === "View Low Inventory") {
+            functions.showLowInventory();
+        } else if (res.managerChoice === "Add to Inventory") {
+            functions.addToInventory();
+        } else if (res.managerChoice === "Add New product") {
+            functions.addNewProduct();
+        } else if (res.managerChoice === "EXIT") {
+            console.clear();
+            process.exit();
+        }
+    });
