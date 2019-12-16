a95c4d739eb13fafafe09140de67189e7a1fcaf9
Adnan Niaz <adnanniaz77@yahoo.com>
2019-12-16 01:58:27 -0800

    added prompt for user choices are required

---
+++ bamazonSupervisor.js
@@ -0,0 +1,36 @@
+const functions = require("./functions.js");
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt({
+        type: "list",
+        name: "supChoice",
+        message: "Choose you option: ",
+        choices: ["View Product Sales by Department", "Create New Department"]
+    })
+    .then(res => {
+        if (res.supChoice === "View Product Sales by Department") {
+            console.log("View Sales");
+            // viewProductSalesByDept();
+        } else if (res.supChoice === "Create New Department") {
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "Enter Department Name: ",
+                        name: "department"
+                    },
+                    {
+                        type: "input",
+                        message: "Enter Overhead Cost: ",
+                        name: "department"
+                    }
+                ])
+                .then(res => {
+                    console.log("New Dept");
+                    // createNewDept(deptName, overHeadCost);
+                });
+        }
+    });
+
+functions.showAllProducts();
